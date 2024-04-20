283cbd0fbb2719a6475a7ef5eb7f2a52b1a89b6a
Melina <85394677+Melina-Singh@users.noreply.github.com>
2024-04-20 23:06:54 +0545

    this is the rotation code of objects in unity

---
+++ test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    Vector3 rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation = new Vector3(0,60,0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotation *Time.deltaTime);  
+        
+    }
+}
